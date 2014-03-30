1ea9e67f2170c5c25f267abcd02fa237d8cc15ff
masnes <mike.asnes@gmail.com>
2014-03-30 15:36:06 -0600

    moved trangle code to right file

---
+++ problem18/triangle.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <climits>
+#include <sstream>
+
+using namespace std;
+
+struct node {
+  node* parent = NULL;
+  node* leftchild = NULL;
+  node* rightchild = NULL;
+  int val = INT_MAX;
+};
+
+int main(void)
+{
+  return 0;
+}
