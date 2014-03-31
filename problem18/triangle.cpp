7bf5b704959241eb3a41a3a1e39c4e11f4582751
masnes <mike.asnes@gmail.com>
2014-03-31 17:31:23 -0600

    Getting relational graph of items to work (in progress)

--- problem18/triangle.cpp
+++ problem18/triangle.cpp
@@ -27,47 +27,58 @@ int main(void) {
   int set;
   // there are 120 numbers in the triangles file
   int val_array[120];
-  int counter = 1;
+  int counter = 0;
   for (int i = 1; i <= 15; i++) {
     for (int j = 1; j <= i; j++) {
       myfile >> set;
       val_array[counter] = set;
-      printf(" %d ", val_array[counter]);
+      cout << counter << ":" << val_array[counter] << "  ";
       counter++;
     }
-    printf("\n");
+    cout << endl;
   }
   /* graph display for each numbers */
-  // 120 numbers, 2 children per number (0 and 1)
-  // array storing the 
+  // 120 numbers, 2 children per number (0 and 1) except for last row
+  // 105 numbers before last row
+  // array storing the offset to the beginning of each line
   int lineoffset[15];
-  counter = 1;
-  for (int i = 0; i <= 14; i++) {
+  counter = 0;
+  for (int i = 0; i <= 15; i++) {
     for (int j = 1; j <= i; j++) {
       counter++;
     }
     lineoffset[i] = counter;
-    // printf("lineoffset at %d = %d \n", i, lineoffset[i]);
+    printf("lineoffset at %d = %d \n", i, lineoffset[i]);
   }
-  int graph[105][1];
-  int line = 1;
+  // only using graph[i][0] and graph[i][1], but the extra padding seems to avoid a bug (array space overflow issues?)
+  int graph[105][2];
+  int line = 0;
   int column = 0;
   // 105 numbers before the last row
-  for (int i = 1; i <= 105; i++) {
+  for (int i = 0; i <= 104; i++) {
     if (column >= line) {
       line++;
       column = 0;
-    } else {
-      column++;
     }
-    printf("line: %d, column: %d\n", line, column);
-    graph[i][0] = lineoffset[line+0] + column - 1;
-    graph[i][1] = lineoffset[line+1] + column;
+    // printf("line: %d, lineoffset: %d, column: %d\n", line, lineoffset[line], column);
+    graph[i-1][0] = lineoffset[line] + column - 1;
+    graph[i-1][1] = lineoffset[line] + column;
+    // if (i > 1)
+    //   cout << graph[i-2][0] << " " << graph[i-2][1] << endl;
+    // cout << graph[i-1][0] << " " << graph[i-1][1] << endl;
+    column++;
   }
-  for (int i = 0; i <= 105; i++) {
+
+  for (int i = 0; i <= 104; i++) {
+    cout << i << ": " << graph[i][0] << " " << graph[i][1] << endl;
+  }
+
+  /*
+  for (int i = 0; i <= 104; i++) {
     printf("graph[%d][%d] = %d | ", i, 0, graph[i][0]);
     printf("graph[%d][%d] = %d\n", i, 1, graph[i][1]);
   }
+  */
 
   /* iterate through array changing values to tree */
   // TODO: iterate array into a tree
